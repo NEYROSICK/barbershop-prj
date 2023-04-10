48e9d80d0b06c7333ec99b7e49572824fe3229bb
NEYROSICK <dhatak24@gmail.com>
2023-04-10 20:33:31 +0300

    Scroll-button-update

--- js/scroll-button.js
+++ js/scroll-button.js
@@ -8,17 +8,11 @@ window.onscroll = function () {
 
 function scrollFunction() {
   if (
-    window.innerHeight + window.pageYOffset >=
-    document.body.offsetHeight - 2
-  ) {
-    mybutton.style.translate = '0 -60px';
-  } else if (
     document.body.scrollTop > 500 ||
     document.documentElement.scrollTop > 500
   ) {
     mybutton.style.opacity = '100%';
     mybutton.style.visibility = 'visible';
-    mybutton.style.translate = '0';
   } else {
     mybutton.style.opacity = '0%';
     mybutton.style.visibility = 'hidden';
